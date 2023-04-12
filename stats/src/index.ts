78823d3a72d7139aba61627bd01b8418cc119f37
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-04-12 15:35:01 +0530

    Formatted the returned response of the csvparser

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,29 +1,10 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
-
-// The following approach has a flow of maybe someone new
-// finds that one of the following variable is unused so delete it.
-//    const homeWin = 'H';
-//    const awayWin = 'A';
-//    const draw = 'D';
-
-// Replace above by a new variable
-//    const matchResult = {
-//      HomeWin: 'H',
-//      AwayWin: 'A',
-//      Draw: 'D'
-//    }
-// But this has also has a flaw and we should use ENUM
-
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
 
 for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
