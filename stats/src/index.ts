f339565867682e56caf413eb8f3cfe929bd5d356
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-04-12 13:48:38 +0530

    Added Enums for better data understanding

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -11,10 +11,30 @@ const matches = fs
 
 let manUnitedWins = 0;
 
+// The following approach has a flow of maybe someone new
+// finds that one of the following variable is unused so delete it.
+//    const homeWin = 'H';
+//    const awayWin = 'A';
+//    const draw = 'D';
+
+// Replace above by a new variable
+//    const matchResult = {
+//      HomeWin: 'H',
+//      AwayWin: 'A',
+//      Draw: 'D'
+//    }
+// But this has also has a flaw and we should use ENUM
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {   
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
